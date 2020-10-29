a9a4986b372d4bb747711fbd2593602080de5fbf
hookSSi <sounghoo12@naver.com>
2020-10-30 01:51:00 +0900

    기본적인 틀 추가
    - Player Controller 추가
    - 시작점, 끝점 임시 추가
    - 기타 쓸만한 리소스들 추가

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public Transform _target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        if(_target != null)
+        {
+            Vector3 targetPos = this.transform.position;
+
+            targetPos.x = _target.position.x;
+            this.transform.position = targetPos;
+        }
+        else
+        {
+            Debug.LogWarning("따라갈 대상이 없습니다.");
+        }
+    }
+}
